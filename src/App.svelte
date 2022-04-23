b6127f74698e54a96c90c4be77f9f0cc05c1417d
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-04-23 18:07:46 -0500

    Add about to routes

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
 
  // page imports
  import Home from './Home/Home.svelte';
+ import About from './About/About.svelte';
 
  // component imports
  import Footer from './Footer.svelte';
@@ -31,7 +32,7 @@
       <ul class="nav">
         <li class="nav-item"><a href="/" class="nav-link cereus-link link-light px-2">Home</a></li>
         <li class="nav-item"><a href="#" class="nav-link cereus-link link-light px-2">Downloads</a></li>
-        <li class="nav-item"><a href="#" class="nav-link cereus-link link-light px-2">About</a></li>
+        <li class="nav-item"><a href="/about" class="nav-link cereus-link link-light px-2">About</a></li>
         <li class="nav-item"><a href="https://sourceforge.net/p/cereus-linux/blog/" target="_blank" rel="noopener noreferrer" class="nav-link cereus-link link-light px-2">Blog</a></li>
       </ul>
     </div>
@@ -40,6 +41,12 @@
 <Route path="/">
     <Home/>
 </Route>
+
+<Route path="/about">
+    <About/>
+</Route>
+
+<Route fallback>Error 404 - not found jsjsjs</Route>
 
 <!-- Footer Component -->
 <Footer/>
