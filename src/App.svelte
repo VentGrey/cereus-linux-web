f0f793d9c631791385013483ddf610ada1213974
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-04-23 17:04:09 -0500

    Work on HOME component

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,9 @@
 	 router
  } from 'tinro';
 
+ // page imports
+ import Home from './Home/Home.svelte';
+
  // component imports
  import Footer from './Footer.svelte';
 
@@ -26,14 +29,17 @@
 <nav class="border-bottom cereus-nav">
     <div class="container d-flex flex-wrap justify-content-end">
       <ul class="nav">
-        <li class="nav-item"><a href="#" class="nav-link cereus-link link-light px-2">Home</a></li>
+        <li class="nav-item"><a href="/" class="nav-link cereus-link link-light px-2">Home</a></li>
         <li class="nav-item"><a href="#" class="nav-link cereus-link link-light px-2">Downloads</a></li>
         <li class="nav-item"><a href="#" class="nav-link cereus-link link-light px-2">About</a></li>
-        <li class="nav-item"><a href="#" class="nav-link cereus-link link-light px-2">Blog</a></li>
+        <li class="nav-item"><a href="https://sourceforge.net/p/cereus-linux/blog/" target="_blank" rel="noopener noreferrer" class="nav-link cereus-link link-light px-2">Blog</a></li>
       </ul>
     </div>
 </nav>
 
+<Route path="/">
+    <Home/>
+</Route>
 
 <!-- Footer Component -->
 <Footer/>
---
+++ src/Home/Home.svelte
@@ -0,0 +1,62 @@
+<script>
+ const editions = [
+     {
+         "icon": "",
+         "name": "LxQt",
+         "link": "/lxqt",
+         "arch": "i686 & amd64",
+         "status": "Testing"
+
+    },
+     {
+        "icon": "",
+         "name": "XFCE",
+         "link": "/xfce",
+        "arch": "i686 & amd64",
+        "status": "Not Available"
+    },
+     {
+        "icon": "",
+         "name": "Plasma",
+         "link": "/plasma",
+        "arch": "amd64 only",
+        "status": "Not Available"
+    },
+     {
+        "icon": "",
+         "name": "Cinnamon",
+         "link": "/cinnamon",
+        "arch": "amd64 only",
+        "status": "Not Available"
+    },
+
+ ];
+</script>
+
+<style>
+ .cereus-text {
+     color: #4DB6AC;
+ }
+
+ .cereus-spin {
+     transition: transform .9s ease-in-out;
+ }
+ .cereus-spin:hover {
+     transform: rotate(360deg);
+ }
+</style>
+
+<section class="container my-2">
+    <center>
+        <a href="https://sourceforge.net/projects/cereus-linux/" target="_blank" rel="noopener noreferrer"><img width="33%" src="/images/textlogo.png"></a>
+        <a href="https://sourceforge.net/projects/cereus-linux/" target="_blank" rel="noopener noreferrer"><img width="11%" class="cereus-spin" src="/images/logo.png"></a>
+
+        <div class="container text-light">
+            <h1>Cereus Linux is a <span class="cereus-text">Void Linux </span>respin focused
+                on giving a user-friendly experience
+            </h1>
+            <h2>We have 4 editions:</h2>
+        </div>
+
+    </center>
+</section>
