21bf76bf066418a68cae377f8e61d7b6bb4fec02
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-04-23 16:03:36 -0500

    Add footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <script>
+ // routing imports
  import {
 	 Route,
 	 router
  } from 'tinro';
 
+ // component imports
+ import Footer from './Footer.svelte';
+
+ // Go to page top when navigating to a new page
  router.subscribe(_ => window.scrollTo(0,0));
 </script>
 
@@ -11,16 +16,24 @@
  .cereus-nav {
      background: #4DB6AC;
  }
+
+ .cereus-link:hover {
+     background-color: #00766b;
+ }
 </style>
 
 
 <nav class="border-bottom cereus-nav">
     <div class="container d-flex flex-wrap justify-content-end">
       <ul class="nav">
-        <li class="nav-item"><a href="#" class="nav-link link-light px-2">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link link-light px-2">Downloads</a></li>
-        <li class="nav-item"><a href="#" class="nav-link link-light px-2">About</a></li>
-        <li class="nav-item"><a href="#" class="nav-link link-light px-2">Blog</a></li>
+        <li class="nav-item"><a href="#" class="nav-link cereus-link link-light px-2">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link cereus-link link-light px-2">Downloads</a></li>
+        <li class="nav-item"><a href="#" class="nav-link cereus-link link-light px-2">About</a></li>
+        <li class="nav-item"><a href="#" class="nav-link cereus-link link-light px-2">Blog</a></li>
       </ul>
     </div>
-  </nav>
+</nav>
+
+
+<!-- Footer Component -->
+<Footer/>
