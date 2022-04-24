de5271efc18453b296004787779aa440436f9b32
Omar Jair Purata Funes <OmarPurataF@gmail.com>
2022-04-23 19:27:54 -0500

    +- finish navbar in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -17,12 +17,26 @@
 </script>
 
 <style>
+ ul, ol {
+     list-style:none;
+ }
+
  .cereus-nav {
      background: #4DB6AC;
  }
 
  .cereus-link:hover {
      background-color: #00766b;
+ }
+
+ .nav li ul {
+     display:none; position:absolute; min-width:120px;
+ }
+
+ .nav li:hover > ul {
+     background-color: #4DB6AC;
+;
+     display:block;
  }
 </style>
 
@@ -31,7 +45,15 @@
     <div class="container d-flex flex-wrap justify-content-end">
       <ul class="nav">
         <li class="nav-item"><a href="/" class="nav-link cereus-link link-light px-2">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link cereus-link link-light px-2">Downloads</a></li>
+        <li class="nav-item">
+            <a href="/" class="nav-link cereus-link link-light px-2">Downloads</a>
+            <ul>
+                <li><a class="text-light text-decoration-none" href="/lxqt">Lxqt</a></li>
+                <li><a class="text-light text-decoration-none" href="/xfce">Xfce</a></li>
+                <li><a class="text-light text-decoration-none" href="/plasma">Plasma</a></li>
+                <li><a class="text-light text-decoration-none" href="/cinnamon">Cinnamon</a></li>
+            </ul>
+        </li>
         <li class="nav-item"><a href="/about" class="nav-link cereus-link link-light px-2">About</a></li>
         <li class="nav-item"><a href="https://sourceforge.net/p/cereus-linux/blog/" target="_blank" rel="noopener noreferrer" class="nav-link cereus-link link-light px-2">Blog</a></li>
       </ul>
